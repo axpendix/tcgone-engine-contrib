b9e4ced3a9b82d814793222e393ea2013a07b9d2
itresad <itrezad@gmail.com>
2018-07-19 19:29:08 +0200

    syntax fix

--- src/tcgwars/logic/impl/gen3/Deoxys.groovy
+++ src/tcgwars/logic/impl/gen3/Deoxys.groovy
@@ -1075,7 +1075,7 @@ public enum Deoxys implements CardInfo {
 					text "If Xatu is Burned or Poisoned by an opponent’s attack (even if Xatu is Knocked Out), the Attacking Pokémon is now affected by the same Special Conditions (1 if there is only 1)."
 					delayedA {
 						after APPLY_SPECIAL_CONDITION,self, {
-							bc "${ef.type}$"
+							bc "${ef.type}"
 						}
 					}
 				}
