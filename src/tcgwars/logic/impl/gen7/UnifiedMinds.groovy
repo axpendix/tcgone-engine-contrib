f19a63b561ca4027e307c3a8d29547399a7aa2a6
Thomas Luong <Luongt@me.com>
2019-09-07 15:51:41 +0700

    Fix UXIE_83 Secret Territory ability

--- src/tcgwars/logic/impl/gen7/UnifiedMinds.groovy
+++ src/tcgwars/logic/impl/gen7/UnifiedMinds.groovy
@@ -2167,10 +2167,9 @@ public enum UnifiedMinds implements CardInfo {
 				weakness P
 				bwAbility "Secret Territory", {
 					text "If you have Mesprit and Azelf in play, apply Weakness for each Pokémon (both yours and your opponent's) as ×4 instead."
-					actionA {
-            getterA (GET_WEAKNESSES) {h->
+            getterA (GET_WEAKNESSES) { h->
               if(self.owner.pbg.all.find{it.name == "Azelf"} && self.owner.pbg.all.find{it.name == "Mespirit"}) {
-                h.object = h.object?.collect{
+                h.object = h.object?.collect {
                   def weakness = it.copy()
                   weakness.feature = "X4"
                   weakness
