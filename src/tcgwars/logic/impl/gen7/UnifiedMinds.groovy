47fce43cbc54d405881c02765b8c8803e15470cf
axpendix <axpendix@hotmail.com>
2019-09-09 23:28:34 +0200

    Implement ORANGURU_182 (to be tested)

--- src/tcgwars/logic/impl/gen7/UnifiedMinds.groovy
+++ src/tcgwars/logic/impl/gen7/UnifiedMinds.groovy
@@ -4351,9 +4351,15 @@ public enum UnifiedMinds implements CardInfo {
 				move "Sage’s Riddle", {
 					text " Put a Pokémon from your hand face down in front of you. Your opponent guesses the type of that Pokémon, and then you reveal it. If your opponent guessed right, they draw 4 cards. If they guessed wrong, you draw 4 cards. Return the Pokémon to your hand."
 					energyCost C
-					attackRequirement {}
+					attackRequirement {
+            assert my.hand.hasType(POKEMON)
+          }
 					onAttack {
-						// TODO:
+            def card = my.hand.filterByType(POKEMON).select()
+            def type = oppChoose(Type.valuesPokemon(), "Sage's Riddle: Your opponent picked a Pokemon card from their hand and wants you to guess that pokemon's type now. If you guess right, you will draw 4 cards, else your opponent will draw 4 cards. Anyway, the card will be returned to your opponent's hand")
+            bc "Guess: $type, actual card was $card, actual card types: ${card.types}"
+            card.showToOpponent("This was the card opponent picked")
+            draw(4, card.first().asPokemonCard().types.contains(type) ? OPPONENT : SELF)
 					}
 				}
 				move "Gentle Slap", {
