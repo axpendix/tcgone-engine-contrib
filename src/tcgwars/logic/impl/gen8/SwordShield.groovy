3433bb0d8ab70e68b2d37c7757c9821ba000b5df
Thomas Luong <luongt@me.com>
2020-03-03 12:59:17 -0800

    Fix ZACIAN_V_138

--- src/tcgwars/logic/impl/gen8/SwordShield.groovy
+++ src/tcgwars/logic/impl/gen8/SwordShield.groovy
@@ -2929,7 +2929,8 @@ public enum SwordShield implements LogicCardInfo {
 					actionA {
             if (confirm("Use Intrepid Sword?")) {
               def topCards = my.deck.subList(0, 3).showToMe("Top 3 cards of your deck")
-              def metalEnergies = topCards.filterByEnergyType(M)
+              def metalEnergies = topCards.filterByBasicEnergyType(W)
+
               if (metalEnergies) {
                 def selectedEnergies = metalEnergies.select(min:0, max:metalEnergies.size(), "Attach any of them to $self")
                 selectedEnergies.each {
