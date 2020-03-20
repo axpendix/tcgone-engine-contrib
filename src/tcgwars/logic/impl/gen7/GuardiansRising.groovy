d69ba6cc15c5bef14b6b89e0a61d7a4fbac6246d
axpendix <axpendix@hotmail.com>
2020-03-21 00:06:04 +0100

    Fix GET_BENCH_SIZE instances to set the value they want directly

--- src/tcgwars/logic/impl/gen7/GuardiansRising.groovy
+++ src/tcgwars/logic/impl/gen7/GuardiansRising.groovy
@@ -1744,7 +1744,7 @@ public enum GuardiansRising implements LogicCardInfo {
             text "Your opponent can't have more than 4 Benched Pokémon. If they have 5 or more Benched Pokémon, they discard Benched Pokémon until they have 4 Pokémon on the Bench. If more than one effect changes the number of Benched Pokémon allowed, use the smaller number."
             getterA (GET_BENCH_SIZE) {h->
               if(h.effect.playerType == self.owner.opposite) {
-                h.object = Math.min(h.object as Integer, 4)
+                h.object = 4
               }
             }
             onActivate {
