f0d80828b7091439cd85268ee47e57b98ffe320a
Thomas Luong <Luongt@me.com>
2020-03-22 12:42:37 -0700

    Fix EXP_ALL_91

--- src/tcgwars/logic/impl/gen3/FireRedLeafGreen.groovy
+++ src/tcgwars/logic/impl/gen3/FireRedLeafGreen.groovy
@@ -2255,8 +2255,8 @@ public enum FireRedLeafGreen implements LogicCardInfo {
               before KNOCKOUT, {
                 if (!self.active && (ef as Knockout).byDamageFromAttack && bg.currentTurn==self.owner.opposite && self.owner.pbg.bench.notEmpty && self.owner.pbg.active.cards.filterByType(BASIC_ENERGY)) {
                   bc "EXP.ALL activates"
-                  if (confirm("move an energy from ${self.owner.pbg.active} to $self ?")) {
-                    def energy = self.cards.filterByType(BASIC_ENERGY).oppSelect("Select the Energy to move")
+                  if (confirm("Move an Energy from ${self.owner.pbg.active} to $self ?")) {
+                    def energy = self.owner.pbg.active.cards.filterByType(BASIC_ENERGY).oppSelect("Select the Energy to move")
                     def tar = self.owner.pbg.all.oppSelect("Select the Pokemon to attach the Energy to")
                     energySwitch(self, tar, energy)
                   }
