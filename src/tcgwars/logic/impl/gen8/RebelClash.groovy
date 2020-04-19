8c9ef4dc1903ed00c16eb73b1f0827b1df9abb4e
scoot404 <thezipcompany@gmail.com>
2020-04-18 21:30:00 -0400

    Fix build

--- src/tcgwars/logic/impl/gen8/RebelClash.groovy
+++ src/tcgwars/logic/impl/gen8/RebelClash.groovy
@@ -305,6 +305,8 @@ public enum RebelClash implements LogicCardInfo {
            onAttack {
              def card = my.discard.findAll{it.basic}.select("Select the card to put on the bench").first()
              my.discard.remove(card)
+            }
+          }
         }
         move "Seed Bomb", {
           text "30 damage."
