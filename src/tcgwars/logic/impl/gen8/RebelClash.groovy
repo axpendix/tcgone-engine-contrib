7031677aa26c3b3b52f50060a255a108061b3231
ufodynasty <39018953+ufodynasty@users.noreply.github.com>
2020-04-21 14:10:43 -0500

    Implemented 40 and 43 (#95)
    * Implemented 40 and 43
    
    * Fixed indenting and typo
    
    imagine being a scrub who uses an ide :P Im stuck using an ipad for another week :(
    
    * Update RebelClash.groovy

--- src/tcgwars/logic/impl/gen8/RebelClash.groovy
+++ src/tcgwars/logic/impl/gen8/RebelClash.groovy
@@ -937,9 +937,14 @@ public enum RebelClash implements LogicCardInfo {
         weakness M
         bwAbility "Playful", {
           text "Once during your turn, when you play this card from your hand to evolve a Pokemon, you may choose an Energy attached to your  opponent’s Active Pokemon and return it to the top of your opponent’s deck."
-          actionA {
-            // TODO
-          }
+            onActivate {r->
+                if(r==PLAY_FROM_HAND && opp.active.cards.filterByType(ENERGY)) {
+                  if(confirm("Use Playful?")) {
+                    powerUsed()
+                    opp.active.cards.filterByType(ENERGY).select("Choose the energy to put on top of their deck").moveTo(addToTop: true, opp.deck)
+                  }
+                }
+              }
         }
         move "Moon Kick", {
           text "60 damage."
@@ -994,9 +999,11 @@ public enum RebelClash implements LogicCardInfo {
         move "Sand Burial", {
           text "Discard a card from the top of your opponent’s deck. If this Pokemon has Cursed Shovel attached to it, discard 2 more cards."
           energyCost C, C
-          attackRequirement {}
+          attackRequirement {assert opp.deck}
           onAttack {
-            // TODO
+            opp.deck.subList(0,1).discard()
+             if (self.cards.findAll { it.name=="Cursed Shovel" }) opp.deck.subList(0,2).discard() 
+
           }
         }
         move "Super Absorption", {
