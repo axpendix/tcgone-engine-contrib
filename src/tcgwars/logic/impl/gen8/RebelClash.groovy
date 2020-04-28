74e7bbe6a7dd0b0c4ac3560be574bffaa6bc5155
ufodynasty <39018953+ufodynasty@users.noreply.github.com>
2020-04-28 13:31:43 -0500

    Fixed counter
    I did a big dumb lastDamage has to be defined :(

--- src/tcgwars/logic/impl/gen8/RebelClash.groovy
+++ src/tcgwars/logic/impl/gen8/RebelClash.groovy
@@ -1036,6 +1036,18 @@ public enum RebelClash implements LogicCardInfo {
       case CINDERACE_VMAX_36:
       return evolution (this, from:"Cinderace V", hp:HP320, type:R, retreatCost:2) {
         weakness W
+        def turnCount=-1
+        HP lastDamage=null
+        customAbility {
+          delayed (priority: LAST) {
+            before APPLY_ATTACK_DAMAGES, {
+              if(bg().currentTurn==self.owner.opposite) {
+                turnCount=bg.turnCount
+                lastDamage=bg().dm().find({it.to==self && it.dmg.value>=0})?.dmg
+              }
+            }
+          }
+        }
         move "Counter", {
           text "30+ damage. This attack does additional damage equal to the amount of damage done to this Pokemon by attacks from your opponent’s Pokemon during your opponent’s last turn."
           energyCost R, C
