cf9c54a9d59dc13ac9fa3928344fb11bd3a936f1
ufodynasty <39018953+ufodynasty@users.noreply.github.com>
2020-04-28 16:34:26 -0500

    Implemented pidove_143 chirp

--- src/tcgwars/logic/impl/gen8/RebelClash.groovy
+++ src/tcgwars/logic/impl/gen8/RebelClash.groovy
@@ -3359,9 +3359,10 @@ public enum RebelClash implements LogicCardInfo {
         move "Chirp", {
           text "Search your deck for up to 2 Pokemon with a [F] Resistance, reveal them, and put them into your hand. Then, shuffle your deck."
           energyCost C
-          attackRequirement {}
+          attackRequirement {
+            assert my.deck : "There are no more cards in your deck."}
           onAttack {
-            // TODO
+            deck.search (max: 2 ,{it.cardTypes.is(POKEMON) &&  it.asPokemonCard().resistance.contains(F)}).showToOpponent("Chosen Pokémon cards.").moveTo(my.hand)
           }
         }
         move "Sharp Wing", {
