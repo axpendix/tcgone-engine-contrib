36ca94ccd218ad9dfbae8aad9feb5ff744642afe
ufodynasty <39018953+ufodynasty@users.noreply.github.com>
2020-04-28 21:15:15 -0500

    reverted changes to pidove
    Now with fewer game engine errors!

--- src/tcgwars/logic/impl/gen8/RebelClash.groovy
+++ src/tcgwars/logic/impl/gen8/RebelClash.groovy
@@ -3363,7 +3363,7 @@ public enum RebelClash implements LogicCardInfo {
             assert my.deck : "There are no more cards in your deck."
           }
           onAttack {
-            deck.search (max: 2 ,{Card it -> it.cardTypes.is(POKEMON) &&  it.asPokemonCard().resistances.contains(F)}).showToOpponent("Chosen Pokémon cards.").moveTo(my.hand)
+            deck.search (max: 2 ,{it.cardTypes.is(POKEMON) && it.asPokemonCard().resistances.contains(F)}).showToOpponent("Chosen Pokémon cards.").moveTo(my.hand)
             shuffleDeck()
           }
         }
