bc3fa580b63922999e092a5946b5c3f1afc368e7
Thomas Luong <Luongt@me.com>
2020-05-02 01:14:17 -0700

    Fix SCOOP_UP_NET_165 maybe

--- src/tcgwars/logic/impl/gen8/RebelClash.groovy
+++ src/tcgwars/logic/impl/gen8/RebelClash.groovy
@@ -3816,9 +3816,9 @@ public enum RebelClash implements LogicCardInfo {
         onPlay {
           def validTargets = my.all.findAll { !it.topPokemonCard.cardTypes.is(VMAX) && !it.topPokemonCard.cardTypes.is(POKEMON_V) && !it.topPokemonCard.cardTypes.is(POKEMON_GX) }
           def tar = validTargets.select("Which Pokemon to put back into your hand?")
-          removePCS(tar)
           tar.cards.getExcludedList(tar.topPokemonCard).discard()
           tar.cards.moveTo(my.hand)
+          removePCS(tar)
         }
         playRequirement {
           assert my.all.findAll { !it.topPokemonCard.cardTypes.is(VMAX) && !it.topPokemonCard.cardTypes.is(POKEMON_V) && !it.topPokemonCard.cardTypes.is(POKEMON_GX) } : "No eligible Pokemon on Bench"
