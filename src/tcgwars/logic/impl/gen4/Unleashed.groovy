a3cde82e93c94ab06d55b2efcf25068594d68bf3
Thomas Luong <Luongt@me.com>
2020-05-04 22:34:12 -0700

    Fix ROSERADE_23 pokePower

--- src/tcgwars/logic/impl/gen4/Unleashed.groovy
+++ src/tcgwars/logic/impl/gen4/Unleashed.groovy
@@ -706,7 +706,7 @@ public enum Unleashed implements LogicCardInfo {
             text "When you attach a [G] Energy card or [P] Energy card from your hand to Roserade during your turn, you may use this power. If you attach a [G] Energy card, the Defending Pokémon is now Confused. If you attach a [P] Energy card, the Defending Pokémon is now Poisoned. This power can’t be used if Roserade is affected by a Special Condition."
             delayedA {
               after ATTACH_ENERGY, self, {
-                if (ef.reason == PLAY_FROM_HAND && checkNoSPC() && (ef.card.asEnergyCard().containsType(G) || ef.card.asEnergyCard().containsType(P)) && confirm("Use Energy Signal?")) {
+                if (ef.reason == PLAY_FROM_HAND && (ef.card.asEnergyCard().containsType(G) || ef.card.asEnergyCard().containsType(P)) && !self.specialConditions && confirm("Use Energy Signal?")) {
                   if (ef.card.asEnergyCard().containsType(G)) {
                     bc "Energy Signal inflicts Confusion"
                     apply CONFUSED, defending
