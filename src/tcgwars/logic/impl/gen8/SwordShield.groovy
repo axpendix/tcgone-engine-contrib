183508028aa58583fe8b07e5d8eb7f5bc0299875
Thomas Luong <Luongt@me.com>
2020-05-05 13:53:38 -0700

    Update AURORA_ENERGY_186 typing

--- src/tcgwars/logic/impl/gen8/SwordShield.groovy
+++ src/tcgwars/logic/impl/gen8/SwordShield.groovy
@@ -3548,12 +3548,19 @@ public enum SwordShield implements LogicCardInfo {
         }
       };
       case AURORA_ENERGY_186:
-      return specialEnergy (this, [[R, D, F, G, W, Y, L, M, P]]) {
+      return specialEnergy (this, [[C]]) {
         text "You can attach this card to 1 of your Pokémon only if you discard another card from your hand." +
           "As long as this card is attached to a Pokémon, it provides every type of Energy but provides only 1 Energy at a time."
         onPlay {reason->
           if (reason == PLAY_FROM_HAND) {
             my.hand.getExcludedList(thisCard).select("Discard").discard()
+          }
+        }
+        getEnergyTypesOverride {
+          if (self != null) {
+            return [[R, D, F, G, W, Y, L, M, P] as Set]
+          } else {
+            return [[C] as Set]
           }
         }
         allowAttach {to->
