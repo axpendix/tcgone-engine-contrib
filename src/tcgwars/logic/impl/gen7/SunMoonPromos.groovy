ed0692f334ad20f60128141aa17b790ec0c8d660
Thomas Luong <Luongt@me.com>
2020-05-12 15:36:34 -0700

    Add null check to TREVENANT_DUSKNOIR_GX_SM217 pale moon gx

--- src/tcgwars/logic/impl/gen7/SunMoonPromos.groovy
+++ src/tcgwars/logic/impl/gen7/SunMoonPromos.groovy
@@ -2828,7 +2828,7 @@ public enum SunMoonPromos implements LogicCardInfo {
               def pcs = defending
               delayed {
                 before BETWEEN_TURNS, {
-                  if (bg.currentTurn == self.owner.opposite) {
+                  if (pcs != null && bg.currentTurn == self.owner.opposite) {
                     bc "Pale Moon GX's effect occurs."
                     new Knockout(pcs).run(bg)
                   }
