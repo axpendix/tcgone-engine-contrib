d18e767b9fb7a28b03ebca3195a3a3232e661b0a
ufodynasty <39018953+ufodynasty@users.noreply.github.com>
2020-05-11 19:01:48 -0500

    Sunflora debugging (#216)
    Changed debug incase it was screwing with the move. Messages didnt seem to happen even though the move activated

--- src/tcgwars/logic/impl/gen7/CosmicEclipse.groovy
+++ src/tcgwars/logic/impl/gen7/CosmicEclipse.groovy
@@ -511,13 +511,13 @@ public enum CosmicEclipse implements LogicCardInfo {
                   eff = getter GET_MOVE_LIST, { h->
                     PokemonCardSet pcs = h.effect.target
                     if (pcs.owner == self.owner && h.object.find { it.types.contains(G) || it.types.contains(R) }) {
-                      bc "$pcs was recognized."
+                      bc "Debug - pokemon"
                       def list = []
                       for (move in h.object){
-                        bc"$move was copied"
                         def copy = move.shallowCopy()
                         copy.energyCost.retainAll()
                         list.add(copy)
+                        bc "Debug - move"
                       }
                       h.object=list
                     }
