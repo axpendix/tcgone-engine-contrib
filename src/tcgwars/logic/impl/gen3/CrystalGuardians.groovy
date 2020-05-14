73948ffd7761228bd516edf564ed23755504acfc
Thomas Luong <Luongt@me.com>
2020-05-14 10:13:22 -0700

    Fix EXPLOUD_EX_92 ability to not damage user

--- src/tcgwars/logic/impl/gen3/CrystalGuardians.groovy
+++ src/tcgwars/logic/impl/gen3/CrystalGuardians.groovy
@@ -2113,7 +2113,7 @@ public enum CrystalGuardians implements LogicCardInfo {
           text "As long as Exploud ex is your Active Pokémon, put 1 damage counter on each of your opponent's Pokémon-ex between turns."
           delayedA {
             before BEGIN_TURN, {
-              if (self.active) {
+              if (self.active && bg.currentTurn == self.owner) {
                 def once = true
                 opp.all.each {
                   if (it.EX) {
