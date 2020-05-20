5458a2d7f95ca8160c8207800b1bbd87b35f3cc0
ufodynasty <39018953+ufodynasty@users.noreply.github.com>
2020-05-20 13:30:25 -0500

    Umbreon delta (#349)

--- src/tcgwars/logic/impl/gen3/DeltaSpecies.groovy
+++ src/tcgwars/logic/impl/gen3/DeltaSpecies.groovy
@@ -758,7 +758,7 @@ public enum DeltaSpecies implements LogicCardInfo {
         pokeBody "Delta Moon", {
           text "When your opponent attaches a Special Energy card from his or her hand to 1 of his or her Pokémon, put 1 damage counter on that Pokémon. You can't use more than 1 Delta Moon Poké-Body each turn."
           delayedA {
-            after PLAY_ENERGY, {
+            after ATTACH_ENERGY, {
               if (ef.cardToPlay.cardTypes.is(SPECIAL_ENERGY) && bg.currentTurn == self.owner.opposite && bg.em().retrieveObject("Delta_Moon") != bg.turnCount) {
                 directDamage 10, ef.resolvedTarget
                 bc "Delta Moon activates"
