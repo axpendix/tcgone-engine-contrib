938fef0d5930be7ad830bafa1eb875dfe46360de
ufodynasty <39018953+ufodynasty@users.noreply.github.com>
2020-05-22 20:02:58 -0500

    Pruning imports 1 (#395)
    * Pruning imports 1
    
    * Update PokemonCardSet.java

--- src/tcgwars/logic/util/PokemonCardSet.java
+++ src/tcgwars/logic/util/PokemonCardSet.java
@@ -1,8 +1,8 @@
 package tcgwars.logic.util;
 
-import static tcgwars.logic.effect.ability.Ability.ActivationReason.*;
-import tcgwars.logic.effect.ability.*;
-import tcgwars.logic.effect.ability.Ability.*;
+
+
+
 
 import com.google.common.collect.ImmutableList;
 import gnu.trove.map.hash.THashMap;
@@ -14,11 +14,13 @@ import tcgwars.logic.PlayerType;
 import tcgwars.logic.card.*;
 import tcgwars.logic.card.energy.EnergyCard;
 import tcgwars.logic.card.pokemon.PokemonCard;
-import tcgwars.logic.effect.ability.Ability;
-import tcgwars.logic.effect.ability.BwAbility;
+import tcgwars.logic.effect.ability.*;
+import tcgwars.logic.effect.ability.Ability.*;
 import tcgwars.logic.effect.getter.*;
 import tcgwars.logic.effect.special.SpecialConditionType;
 import tcgwars.logic.groovy.TcgStatics;
+
+import static tcgwars.logic.effect.ability.Ability.ActivationReason.*;
 
 import java.io.Serializable;
 import java.util.*;
