e0001b1b53c6283b36f4035dc0453bc55226508b
Vincent Cunningham <v.cunningham@flagrama.com>
2020-05-23 00:36:27 -0400

    Omanyte 56 attempt revert effect after affected card discarded

--- src/tcgwars/logic/impl/gen3/PowerKeepers.groovy
+++ src/tcgwars/logic/impl/gen3/PowerKeepers.groovy
@@ -1613,6 +1613,13 @@ public enum PowerKeepers implements LogicCardInfo {
                 my.deck.remove(it)
                 it.cardTypes.add(BASIC)
                 it.cardTypes.remove(EVOLUTION)
+                delayed {
+                  after REMOVE_FROM_PLAY, it {
+                    it.cardTypes.remove(BASIC)
+                    it.cardTypes.add(EVOLUTION)
+                    unregister()
+                  }
+                }
                 benchPCS(it)
               }
               shuffleDeck()
