375d120a8425d33b9b804ca5c07c4ff78d04ff80
ufodynasty <39018953+ufodynasty@users.noreply.github.com>
2020-05-30 17:29:32 -0500

    Fix build maybe (#467)
    The metronome code looked good so im guessing there was a missing import. It wasn’t obvious to me which import was missing so I included everything that was in pokemod that wasn’t in this as the same code works properly there.

--- src/tcgwars/logic/impl/gen4/DiamondPearl.groovy
+++ src/tcgwars/logic/impl/gen4/DiamondPearl.groovy
@@ -8,10 +8,29 @@ import static tcgwars.logic.card.Type.*;
 import static tcgwars.logic.card.CardType.*;
 import static tcgwars.logic.groovy.TcgBuilders.*;
 import static tcgwars.logic.groovy.TcgStatics.*
+import static tcgwars.logic.effect.ability.Ability.ActivationReason.*
+import static tcgwars.logic.effect.EffectType.*;
+import static tcgwars.logic.effect.Source.*;
+import static tcgwars.logic.effect.EffectPriority.*
+import static tcgwars.logic.effect.special.SpecialConditionType.*
 import static tcgwars.logic.card.Resistance.ResistanceType.*
 import static tcgwars.logic.card.Weakness.*
 
-import tcgwars.logic.card.*
+import java.util.*;
+import tcgwars.entity.*;
+import tcgwars.logic.*;
+import tcgwars.logic.card.*;
+import tcgwars.logic.card.energy.*;
+import tcgwars.logic.card.pokemon.*;
+import tcgwars.logic.card.trainer.*;
+import tcgwars.logic.effect.*;
+import tcgwars.logic.effect.ability.*;
+import tcgwars.logic.effect.advanced.*;
+import tcgwars.logic.effect.basic.*;
+import tcgwars.logic.effect.blocking.*;
+import tcgwars.logic.effect.event.*;
+import tcgwars.logic.effect.getter.*;
+import tcgwars.logic.effect.special.*;
 import tcgwars.logic.util.*;
 
 /**
