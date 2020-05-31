0fe6cc80e51ed14709c8a392bf72b39fef1c92a5
ufodynasty <39018953+ufodynasty@users.noreply.github.com>
2020-05-31 17:15:03 -0500

    Implement Charizard EX burn down (#483)
    * Implement Charizard EX burn down
    
    Harder than expected with the nonstandard no weakness,resistance,or effects.
    
    * Implemented Energy Trans
    
    Thats the last of the easy TODOS
    I think I can do buzzap but porygon is gonna be rough

--- src/tcgwars/logic/impl/pokemod/PokemodBaseSet.groovy
+++ src/tcgwars/logic/impl/pokemod/PokemodBaseSet.groovy
@@ -2080,8 +2080,8 @@ public enum PokemodBaseSet implements LogicCardInfo {
           text "200 damage. Discard 5 [R] Energy attached to Charizard ex. This attack's damage isn't affected by Weakness, Resistance, Poké-Powers, Poké-Bodies, and any other effects on the Defending Pokémon."
           energyCost R, R, R, R, R
           onAttack {
-            damage 200
-            // TODO:
+            discardSelfEnergy(R,R,R,R,R)
+            new ResolvedDamage(hp(200), my.active, opp.active, Source.ATTACK, DamageManager.DamageFlag.NO_RESISTANCE, DamageManager.DamageFlag.NO_WEAKNESS, DamageManager.DamageFlag.NO_DEFENDING_EFFECT).run(bg)
           }
         }
       };
@@ -2119,7 +2119,13 @@ public enum PokemodBaseSet implements LogicCardInfo {
         pokePower "Energy Trans", {
           text "As often as you like during your turn (before your attack), move a [G] Energy card attached to 1 of your Pokémon to another of your Pokémon. This power can't be used if Venusaur ex is affected by a Special Condition."
           actionA {
-            // TODO:
+            checkNoSPC()
+            assert my.all.findAll{it.cards.filterByEnergyType(G)} : "You have no [G] Energy attached to your pokemon"
+            assert my.all.size() >= 2 : "You only have one Pokémon in play."
+            powerUsed()
+            def src = my.all.findAll{it.cards.filterByEnergyType(G)}.select("Move Energy from which pokemon?")
+            def tar = my.all.findAll {it != src}.select("Move Energy to which Pokémon?")
+            src.cards.filterByEnergyType(G).select("Select a Basic Energy card to move to the target.").each{energySwitch(src,tar,it)}
           }
         }
         move "Pollen Hazard", {
