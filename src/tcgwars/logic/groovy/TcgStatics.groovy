5caeb62b681634582f0cec6ede7c4c26fba8d251
Guillermo Bianchi <ultraguille2006@gmail.com>
2020-06-04 14:40:03 -0300

    Debug print for Fossil Excavator (MT 111) and potential fix for crash-on-click-on-search

--- src/tcgwars/logic/groovy/TcgStatics.groovy
+++ src/tcgwars/logic/groovy/TcgStatics.groovy
@@ -1185,10 +1185,14 @@ class TcgStatics {
   }
 
   static boolean isValidFossilCard(Card potentialFossil){
+    if (potentialFossil.cardTypes.is(STAGE2)){
+      def debug_test = bg.gm().getBasicsFromStage2(potentialFossil.name)
+      bc "${debug_test}"
+    }
     if(
       (potentialFossil.cardTypes.is(ITEM) && potentialFossil.name.contains("Fossil")) ||
       (potentialFossil.cardTypes.is(STAGE1) && potentialFossil.predecessor.contains("Fossil")) ||
-      (potentialFossil.cardTypes.is(STAGE2) && bg.gm().getBasicsFromStage2(potentialFossil.name).findAll{it.contains("Fossil")})
+      (potentialFossil.cardTypes.is(STAGE2) && bg.gm().getBasicsFromStage2(potentialFossil.name).any{it.contains("Fossil")})
     )
       return true
     else
