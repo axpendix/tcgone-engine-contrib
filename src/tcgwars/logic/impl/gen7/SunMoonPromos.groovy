9a64f1bd9a7bd844a8d268e89c3ee97ec55276d3
Vincent Cunningham <flagrama@users.noreply.github.com>
2020-06-05 19:56:54 -0400

    Trev/Dusk GX SM217 - Attempt to fix Pale Moon GX (#571)
    Pale Moon GX currently affects Pokémon that are ignoring effects from
    attacks.

--- src/tcgwars/logic/impl/gen7/SunMoonPromos.groovy
+++ src/tcgwars/logic/impl/gen7/SunMoonPromos.groovy
@@ -3002,7 +3002,7 @@ public enum SunMoonPromos implements LogicCardInfo {
 
               def pcs = defending
               delayed {
-                before BETWEEN_TURNS, {
+                before BETWEEN_TURNS, pcs, Source.ATTACK, {
                   if (pcs.topPokemonCard != null && bg.currentTurn == self.owner.opposite) {
                     bc "Pale Moon GX's effect occurs."
                     new Knockout(pcs).run(bg)
