4abccdfcc2d07a0665a5b8263c9a8bea09fd3d23
Guillermo Bianchi <ultraguille2006@gmail.com>
2020-06-04 21:30:30 -0300

    Fixed extra prize priority in Whimsicott (GRI 91)

--- src/tcgwars/logic/impl/gen7/GuardiansRising.groovy
+++ src/tcgwars/logic/impl/gen7/GuardiansRising.groovy
@@ -2282,7 +2282,7 @@ public enum GuardiansRising implements LogicCardInfo {
             text "If the Defending Pokémon is Knocked Out during your next turn, take 2 more Prize cards."
             energyCost C
             onAttack {
-              delayed {
+              delayed (priority: LAST) {
                 def pcs = defending
                 after KNOCKOUT, pcs, {
                   if(turnCount + 2 == bg.turnCount) {
