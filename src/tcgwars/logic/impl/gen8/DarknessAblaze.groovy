022970e51242b1bab358bd194fa934da7f4a659a
Vincent Cunningham <v.cunningham@flagrama.com>
2020-06-12 19:43:26 -0400

    WIMP_17 DAB - Implement Gnaw and Run attack

--- src/tcgwars/logic/impl/gen8/DarknessAblaze.groovy
+++ src/tcgwars/logic/impl/gen8/DarknessAblaze.groovy
@@ -641,6 +641,7 @@ public enum DarknessAblaze implements LogicCardInfo {
           attackRequirement {}
           onAttack {
             damage 10
+            switchYourActive
           }
         }
       };
