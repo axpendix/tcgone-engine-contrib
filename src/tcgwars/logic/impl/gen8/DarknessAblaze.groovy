d2b7936b4322f2d959dc8083a4eb19df5eba0b33
Vincent Cunningham <v.cunningham@flagrama.com>
2020-06-13 00:48:22 -0400

    ROWL_11 DAB - Workaround?

--- src/tcgwars/logic/impl/gen8/DarknessAblaze.groovy
+++ src/tcgwars/logic/impl/gen8/DarknessAblaze.groovy
@@ -459,12 +459,8 @@ public enum DarknessAblaze implements LogicCardInfo {
           text "If you played Bird Keeper from your hand during this turn, ignore all Energy in the attack costs of this Pokémon."
           delayedA {
             before ENERGY_COST_CALCULATOR, {
-              def fields = ef.getClass().getDeclaredFields()
-              fields.each {
-                bc it.toString()
-              }
-              bc fields
-              if (ef.attacker == self && bg.currentTurn == self.owner && bg.em().retrieveObject("Rowlet_Sky_Circus_$self.owner") == bg.turnCount) {
+              // TODO: Figure out how to do this right, or if this is already right
+              if (self.active && bg.currentTurn == self.owner && bg.em().retrieveObject("Rowlet_Sky_Circus_$self.owner") == bg.turnCount) {
                 bc "Sky Circus ignores Energy cost for $ef.attacker's $ef.move"
                 prevent()
               }
