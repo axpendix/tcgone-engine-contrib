38b48bd71d0f4b84bb900b9c94f1be3d43a49b5b
Vincent Cunningham <flagrama@users.noreply.github.com>
2020-06-12 23:21:00 -0400

    Darkness ablaze quick fixes n+1 (#662)
    * ROWL_11 DAB - Prevent ENERGY_COST_CALCULATOR
    
    * ROWL_11 DAB - Remove spaces from object key

--- src/tcgwars/logic/impl/gen8/DarknessAblaze.groovy
+++ src/tcgwars/logic/impl/gen8/DarknessAblaze.groovy
@@ -458,12 +458,9 @@ public enum DarknessAblaze implements LogicCardInfo {
         bwAbility "Sky Circus", {
           text "If you played Bird Keeper from your hand during this turn, ignore all Energy in the attack costs of this Pokémon."
           delayedA {
-            before CHECK_ATTACK_REQUIREMENTS, {
-              if (ef.attacker == self && bg.currentTurn == self.owner && bg.em().retrieveObject("Rowlet Sky Circus $self.owner") == bg.turnCount) {
+            before ENERGY_COST_CALCULATOR, {
+              if (ef.attacker == self && bg.currentTurn == self.owner && bg.em().retrieveObject("Rowlet_Sky_Circus_$self.owner") == bg.turnCount) {
                 bc "Sky Circus ignores Energy cost for $ef.attacker's $ef.move"
-                def copy = ef.move.shallowCopy()
-                copy.energyCost.clear()
-                attack (copy as Move)
                 prevent()
               }
             }
@@ -489,7 +486,7 @@ public enum DarknessAblaze implements LogicCardInfo {
               }
               after PLAY_TRAINER, {
                 if (flag) {
-                  bg.em().storeObject("Rowlet Sky Circus $thisCard.player", bg.turnCount)
+                  bg.em().storeObject("Rowlet_Sky_Circus_$thisCard.player", bg.turnCount)
                   flag = false
                 }
               }
