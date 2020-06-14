3487ef42b5bfe44d087156136629929f5f97cb0b
ufodynasty <ufodynasty12@gmail.com>
2020-06-13 21:36:29 -0500

    testing NORMAL param on self move getters

--- src/tcgwars/logic/impl/gen7/CosmicEclipse.groovy
+++ src/tcgwars/logic/impl/gen7/CosmicEclipse.groovy
@@ -505,7 +505,7 @@ public enum CosmicEclipse implements LogicCardInfo {
             text "During your next turn, ignore all Energy in the attack costs of [G] Pokémon and [R] Pokémon. (This includes Pokémon that come into play on that turn.)"
             energyCost C, C
             onAttack {
-              getter GET_MOVE_LIST, {h->
+              getter GET_MOVE_LIST, NORMAL, {h->
                 PokemonCardSet pcs = h.effect.target
                 if(bg.currentTurn == self.owner && (pcs.types.contains(G)||pcs.types.contains(R)) && pcs.owner == self.owner){
                   def list=[]
@@ -1831,17 +1831,19 @@ public enum CosmicEclipse implements LogicCardInfo {
           resistance M, MINUS20
           bwAbility "Speed Cheer", {
             text "The attacks of your Pokémon-GX in play that evolve from Eevee cost [C] less. You can't apply more than 1 Speed Cheer Ability at a time."
-            delayedA {
-              before CHECK_ATTACK_REQUIREMENTS, {
-                if(ef.attacker.owner == self.owner && ef.attacker.pokemonGX && ef.attacker.topPokemonCard.realEvolution && ef.attacker.topPokemonCard.predecessor == "Eevee" && bg.currentTurn == self.owner && bg.em().retrieveObject("Speed_Cheer") != bg.turnCount) {
-                  bg.em().storeObject("Speed_Cheer", bg.turnCount)
-                  def copy = ef.move.shallowCopy()
+            getterA GET_MOVE_LIST, NORMAL, {h->
+              PokemonCardSet pcs = h.effect.target
+              if(pcs.owner==self.owner && pcs.pokemonGX && pcs.topPokemonCard.realEvolution && pcs.topPokemonCard.predecessor == "Eevee" && bg.currentTurn == self.owner && bg.em().retrieveObject("Speed_Cheer") != bg.turnCount){
+                def list=[]
+                for(move in h.object){
+                  def copy=move.shallowCopy()
                   if (copy.energyCost.contains(C)) {
                     copy.energyCost.remove(C)
                   }
-                  attack (copy as Move)
-                  prevent()
+                  list.add(copy)
                 }
+                h.object=list
+                bg.em().storeObject("Speed Cheer", bg.turnCount)
               }
             }
           }
