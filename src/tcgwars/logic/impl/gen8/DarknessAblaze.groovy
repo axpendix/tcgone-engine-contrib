8a2c855ee6f63385f0228dd479bd0f535f720cbb
Vincent Cunningham <flagrama@users.noreply.github.com>
2020-06-16 16:49:30 -0400

    Darkness ablaze quick fixes & Debug (#723)
    * GALA_MR_R_38 DAB - Add debug strings
    
    * SUIC_39 DAB - Try fix Aurora Loop
    
    * RELI_42 DAB - Remove fossil from deck
    
    * WISH_52 DAB - Shuffle deck
    
    * DRAC_55 DAB - Remove hand check for testing purposes

--- src/tcgwars/logic/impl/gen8/DarknessAblaze.groovy
+++ src/tcgwars/logic/impl/gen8/DarknessAblaze.groovy
@@ -1051,8 +1051,10 @@ public enum DarknessAblaze implements LogicCardInfo {
 
             def info = "Select opponent's Prize card to switch with the top card from their deck."
             def oppTopDeck = opp.deck.remove(0)
+            bc "$oppTopDeck removed from top of deck"
             def oppPrize = opp.prizeCardSet.faceDownCards.select(hidden: true, info)
             def oppPrizeIndex = opp.prizeCardSet.faceDownCards.indexOf(oppPrize.first())
+            bc "$oppPrize at index $oppPrizeIndex chosen from prize cards"
             opp.prizeCardSet.faceDownCards.set(oppPrizeIndex, oppTopDeck)
             oppPrize.moveTo(addToTop: true, opp.deck)
           }
@@ -1086,12 +1088,12 @@ public enum DarknessAblaze implements LogicCardInfo {
             afterDamage {
               def waterE = self.cards.energyCount W
               if (waterE) {
-                def energies = []
+                def energies = new CardList()
                 def count = Math.min waterE, 2
                 def finalCount = 0
                 count.times {
                   def info = "Select [W] Energy to return to your hand."
-                  def energy = self.cards.findAll {energyFilter W}.select(info)
+                  def energy = self.cards.findAll {energyFilter W}.getExcludedList(energies).select(info)
                   if (energy instanceof SpecialEnergyCard) {
                     def types = energy.getEnergyTypesOverride()
                     types.each {
@@ -1106,9 +1108,7 @@ public enum DarknessAblaze implements LogicCardInfo {
                   energies.add(energy)
                   if (finalCount >= count) return
                 }
-                energies.each {
-                  self.cards[self.cards.indexOf(it)].moveTo(my.hand)
-                }
+                energies.moveTo my.hand
               }
             }
           }
@@ -1163,6 +1163,7 @@ public enum DarknessAblaze implements LogicCardInfo {
           onAttack {
             def info = "Select up to 2 Rare Fossil cards to put on your bench."
             my.deck.search(max:2, info, {it.name == "Rare Fossil"}).each {fossil ->
+              my.deck.remove(fossil)
               bg.em().run(new PlayTrainer(fossil))
             }
             shuffleDeck()
@@ -1339,6 +1340,7 @@ public enum DarknessAblaze implements LogicCardInfo {
           }
           onAttack {
             my.hand.moveTo hidden:true, my.deck
+            shuffleDeck()
             draw 8
           }
         }
@@ -1405,7 +1407,7 @@ public enum DarknessAblaze implements LogicCardInfo {
           text "If this Pokémon is your Active Pokémon, your opponent can’t play any Pokémon from their hand to evolve their Pokémon."
           delayedA {
             before PREVENT_EVOLVE, {
-              if (self.active && bg.currentTurn == self.owner.opposite && ef.evolutionCard.player.pbg.hand.contains(ef.evolutionCard)) {
+              if (self.active && bg.currentTurn == self.owner.opposite) {
                 return true
               }
             }
