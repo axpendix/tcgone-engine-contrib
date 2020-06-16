b89d7ee0e05ed2888e07b2c1e77f04b7e4996fb1
Vincent Cunningham <v.cunningham@flagrama.com>
2020-06-16 19:49:08 -0400

    DRAC_55 DAB - Add informative message

--- src/tcgwars/logic/impl/gen8/DarknessAblaze.groovy
+++ src/tcgwars/logic/impl/gen8/DarknessAblaze.groovy
@@ -1404,6 +1404,7 @@ public enum DarknessAblaze implements LogicCardInfo {
           delayedA {
             before PLAY_EVOLUTION, {
               if (self.active && bg.currentTurn == self.owner.opposite && ef.cardToPlay.player.pbg.hand.contains(ef.cardToPlay)) {
+                wcu "Primal Law prevents playing Pokémon from your hand to evolve your Pokémon"
                 prevent()
               }
             }
