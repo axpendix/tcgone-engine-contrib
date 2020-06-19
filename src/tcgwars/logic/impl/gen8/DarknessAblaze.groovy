ccf2e1abd76e5ab0986a76ee0ab5ebf5b9e3e481
axpendix <axpendix@hotmail.com>
2020-06-20 00:32:26 +0200

    Update src/tcgwars/logic/impl/gen8/DarknessAblaze.groovy
    Use `multiSelect` method

--- src/tcgwars/logic/impl/gen8/DarknessAblaze.groovy
+++ src/tcgwars/logic/impl/gen8/DarknessAblaze.groovy
@@ -3767,12 +3767,7 @@ public enum DarknessAblaze implements LogicCardInfo {
       return supporter (this) {
         text "Choose up to 2 of your Pokémon in play. Search your deck for a card that evolves from each of those Pokémon and put it on top of the Pokémon to evolve it. Then, shuffle your deck. (You can’t use this card during your first turn or on a Pokémon that just came into play.)"
         onPlay {
-          def pcsList = my.all.findAll {it.turnCount < bg.turnCount}
-          if (pcsList.size() > 1) {
-            pcsList = pcsList.select(max:2, "Choose up to 2 of your Pokémon in play that can make a regular evolve during this turn.")
-          }
-
-          pcsList.each{ pcs =>
+          multiSelect (my.all.findAll {it.turnCount < bg.turnCount}, 2, "Choose up to 2 of your Pokémon in play that can make a regular evolve during this turn.").each { pcs =>
             def sel = deck.search ("Select a Pokémon that evolves from $pcs.name.", {
               it.cardTypes.is(EVOLUTION) && it.predecessor == pcs.name
             })
