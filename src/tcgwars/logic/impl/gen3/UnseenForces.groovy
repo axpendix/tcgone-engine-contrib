1a05e697b1c50b6013359f3da449565180714ed0
starg09 <ultraguille2006@gmail.com>
2020-06-26 15:34:46 -0300

    More Debug on Lugia ex (#829)

--- src/tcgwars/logic/impl/gen3/UnseenForces.groovy
+++ src/tcgwars/logic/impl/gen3/UnseenForces.groovy
@@ -2649,11 +2649,13 @@ public enum UnseenForces implements LogicCardInfo {
                 def optionsNum = (0..potentialEnergy.size() -1).toList()
                 def options = potentialEnergy.findAll{it[0].contains(enReq[0])}
                 def cnt = 0
-                bc ">Options available: ${potentialEnergy.size()}"
+                bc ">Options available: ${options.size()}"
                 bc ">${enReq[0]} Energy selected: ${cnt}/${enReq[1]}"
                 while (cnt < enReq[1]){
                   if (options){
                     def chosenEnergy = 0
+                    bc ">>${options.collect{it[2]}}"
+                    bc ">>${options.collect{it[2]}.flatten()}"
                     if (options.size() > 1) {
                       chosenEnergy = choose(optionsNum,options.collect{it[2]}, "Discard a ${enReq[0]} Energy from these options:")
                     }
