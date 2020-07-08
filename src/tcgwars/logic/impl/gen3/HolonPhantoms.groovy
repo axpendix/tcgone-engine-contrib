6732bae9b3f3ce8b5710233364f0040641ebecb3
Guillermo Bianchi <ultraguille2006@gmail.com>
2020-07-08 01:46:15 -0300

    Added a TODO on Delta Supply re: Cursed Glare

--- src/tcgwars/logic/impl/gen3/HolonPhantoms.groovy
+++ src/tcgwars/logic/impl/gen3/HolonPhantoms.groovy
@@ -428,6 +428,7 @@ public enum HolonPhantoms implements LogicCardInfo {
             checkLastTurn()
             assert my.hand.filterByType(BASIC_ENERGY) || my.hand.findAll{it.name.contains("δ Rainbow Energy")}: "No Basic Energy or δ Rainbow Energy in Hand"
             assert my.all.findAll { it.topPokemonCard.cardTypes.is(DELTA) } : "No Delta Pokemon in play"
+            //TODO: Handle Cursed Glare, shouldn't be allowed to attach in that case.
             powerUsed()
             def energy = my.hand.findAll({it.cardTypes.is(BASIC_ENERGY) || it.name == "δ Rainbow Energy"}).select("Select an energy to attach to one of your δ Pokemon").first()
             def tar = my.all.findAll { it.topPokemonCard.cardTypes.is(DELTA) }.select("Select a δ Pokemon to attach the Energy to.")
