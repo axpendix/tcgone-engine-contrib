744850b4e6fc52c201a88cf0adc5539a34effbc0
Guillermo Bianchi <ultraguille2006@gmail.com>
2020-07-09 02:29:57 -0300

    Reverse Vaporeon (CEC)
    Caught in the middle of other refactors, this one didn't need it.

--- src/tcgwars/logic/impl/gen7/CosmicEclipse.groovy
+++ src/tcgwars/logic/impl/gen7/CosmicEclipse.groovy
@@ -1252,12 +1252,25 @@ public enum CosmicEclipse implements LogicCardInfo {
         return evolution (this, from:"Eevee", hp:HP110, type:W, retreatCost:2) {
           weakness G
           bwAbility "Vitality Cheer", {
+            def target = []
+            def source = []
+            bg.em().storeObject("Vitality_Cheer_target", target)
+            bg.em().storeObject("Vitality_Cheer_source", source)
             text "Your Pokémon-GX in play that evolve from Eevee get +60 HP. You can't apply more than 1 Vitality Cheer Ability at a time."
             getterA (GET_FULL_HP) {h->
               def pcs = h.effect.target
-              if(keyStore("Vitality_Cheer", pcs, null) != bg.turnCount && pcs.owner==self.owner && pcs.pokemonGX && pcs.realEvolution && pcs.topPokemonCard.predecessor == "Eevee"){
-                h.object += hp(60)
-                keyStore("Vitality_Cheer", pcs, bg.turnCount)
+              if (pcs.owner == self.owner && pcs.pokemonGX && pcs.topPokemonCard.cardTypes.is(EVOLUTION) && pcs.topPokemonCard.predecessor == "Eevee"){
+                target = bg.em().retrieveObject("Vitality_Cheer_target")
+                source = bg.em().retrieveObject("Vitality_Cheer_source")
+                if(!target.contains(pcs)){
+                  h.object += hp(60)
+                  target.add(pcs)
+                  bg.em().storeObject("Vitality_Cheer_target", target)
+                  source.add(self)
+                  bg.em().storeObject("Vitality_Cheer_source", source)
+                } else if(source.get(target.indexOf(pcs)) == self){
+                  h.object += hp(60)
+                }
               }
             }
           }
