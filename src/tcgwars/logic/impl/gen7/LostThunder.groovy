10f02f22de6f3f80332a3907db29da65f40653dd
Guillermo Bianchi <ultraguille2006@gmail.com>
2020-07-16 19:49:22 -0300

    Fix a missing check
    Makes every card unplayable with a tyra gx in play otherwise.

--- src/tcgwars/logic/impl/gen7/LostThunder.groovy
+++ src/tcgwars/logic/impl/gen7/LostThunder.groovy
@@ -2924,7 +2924,7 @@ public enum LostThunder implements LogicCardInfo {
                 }
               }
               before MOVE_CARD, {
-                if (flag.contains(card) && ef.newLocation?.is(card.player.pbg.discard)) {
+                if (flag != null && flag.contains(card) && ef.newLocation?.is(card.player.pbg.discard)) {
                   bc "debug test"
                   prevent()
                 }
