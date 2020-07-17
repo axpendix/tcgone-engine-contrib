c7e2da6b2ffa1035162acdc78a7880388255bb11
Guillermo Bianchi <ultraguille2006@gmail.com>
2020-07-16 21:10:25 -0300

    Remove the changed_impl element before next turn begins
    Should be more than enough for all checks that may need to access that info to be finished.

--- src/tcgwars/logic/impl/gen7/CosmicEclipse.groovy
+++ src/tcgwars/logic/impl/gen7/CosmicEclipse.groovy
@@ -4735,6 +4735,12 @@ public enum CosmicEclipse implements LogicCardInfo {
                           bg.em().retrieveAndStore("impl_changed_cards", {it ?: []})
                           bg.em().run(new ChangeImplementation(trainerCard, pokemonCard))
                           bg.em().retrieveAndStore("impl_changed_cards", {it.add([pokemonCard, trainerCard]); it})
+                          delayed {
+                            before BEGIN_TURN, {
+                              bg.em().retrieveAndStore("impl_changed_cards", {it.remove([pokemonCard, trainerCard]); it})
+                              unregister()
+                            }
+                          }
                           unregister()
                           ef2 = null
                         }
