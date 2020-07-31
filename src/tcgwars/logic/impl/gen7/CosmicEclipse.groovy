4e28d4691b06ffb3ba8bafe7a16bf31d263cf5e5
Guillermo Bianchi <ultraguille2006@gmail.com>
2020-07-31 01:16:06 -0300

    Shorter Action Name for Lillie's Poké Doll

--- src/tcgwars/logic/impl/gen7/CosmicEclipse.groovy
+++ src/tcgwars/logic/impl/gen7/CosmicEclipse.groovy
@@ -4748,7 +4748,7 @@ public enum CosmicEclipse implements LogicCardInfo {
                       }
                     }
                   }
-                  acl = action("Send Lillie's Poké Doll into Deck Bottom", [TargetPlayer.SELF]) {
+                  acl = action("Put Lillie's Poké Doll on Deck ↓", [TargetPlayer.SELF]) {
                     assert self.active : "Lillie's Poké Doll must be the Active Pokémon."
                     self.cards.getExcludedList(self.topPokemonCard).discard()
                     self.cards.moveTo(my.deck)
