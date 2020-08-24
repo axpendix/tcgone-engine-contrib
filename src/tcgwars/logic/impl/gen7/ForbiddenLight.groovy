173ed5d70ae31a66b556924544fa939d0c29c3c1
Guillermo Bianchi <ultraguille2006@gmail.com>
2020-08-24 17:52:38 -0300

    Fix Metal Frying Pan (FLI)
    Should only prevent damage done by opponent's attacks (so Steelix LOT should not reduce its self damage)

--- src/tcgwars/logic/impl/gen7/ForbiddenLight.groovy
+++ src/tcgwars/logic/impl/gen7/ForbiddenLight.groovy
@@ -2752,10 +2752,12 @@ public enum ForbiddenLight implements LogicCardInfo {
             }
             eff2 = delayed{
               before APPLY_ATTACK_DAMAGES, {
-                bg.dm().each{
-                  if(it.to == self && self.types.contains(M) && it.dmg.value && it.notNoEffect) {
+                if (ef.attacker.owner != self.owner){
+                  bg.dm().each{
+                    if(it.to == self && self.types.contains(M) && it.dmg.value && it.notNoEffect) {
                       bc "Metal Frying Pan -30"
                       it.dmg-=hp(30)
+                    }
                   }
                 }
               }
