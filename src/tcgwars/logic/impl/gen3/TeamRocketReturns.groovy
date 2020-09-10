804db6eb52b4e790a9ce56bab1f32bf45300fe50
Guillermo Bianchi <ultraguille2006@gmail.com>
2020-09-10 01:49:34 -0300

    Fix Rocket's admin misinteraction with Milotic Delta

--- src/tcgwars/logic/impl/gen3/TeamRocketReturns.groovy
+++ src/tcgwars/logic/impl/gen3/TeamRocketReturns.groovy
@@ -2320,7 +2320,7 @@ public enum TeamRocketReturns implements LogicCardInfo {
           text "You can play only one Supporter card each turn. When you play this card, put it next to your Active Pokémon. When your turn ends, discard this card.\nEach player shuffles his or her hand into his or her deck. Then, each player counts his or her Prize cards left and draws up to that many cards. (You draw your cards first.)"
           onPlay {
             my.hand.getExcludedList(thisCard).moveTo(hidden:true, my.deck)
-            opp.hand.getExcludedList(thisCard).moveTo(hidden:true, opp.deck)
+            opp.hand.moveTo(hidden:true, opp.deck)
             shuffleDeck()
             shuffleDeck(null,TargetPlayer.OPPONENT)
             draw choose(1..my.prizeCardSet.size(),"How many cards would you like to draw?")
