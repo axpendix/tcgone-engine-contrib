c67fd2a286d6079faac25a07dcf2d2841c523d0f
Thomas Luong <Luongt@me.com>
2020-09-16 13:31:23 -0700

    Wrap getWholeName in a try catch

--- src/tcgwars/logic/util/PokemonCardSet.java
+++ src/tcgwars/logic/util/PokemonCardSet.java
@@ -285,7 +285,14 @@ public class PokemonCardSet implements PokemonStack, Serializable {
     }
   }
 
-  public String getWholeName() { return getTopPokemonCard().getWholeName(); }
+  public String getWholeName() {
+    try {
+      lastName = getTopPokemonCard().getWholeName();
+      return lastName;
+    } catch (Exception e) {
+      return lastName;
+    }
+  }
 
   public boolean isActive() {
     return TcgStatics.bg().getPBG(owner).getActive() == this;
