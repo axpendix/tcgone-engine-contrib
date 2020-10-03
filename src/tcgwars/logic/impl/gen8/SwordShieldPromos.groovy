28fa5e4a492059d17cd3b4a6607d93cabe7c165a
Vincent Cunningham <v.cunningham@flagrama.com>
2020-10-03 13:37:11 -0400

    Prune down to only the released ones

--- src/tcgwars/logic/impl/gen8/SwordShieldPromos.groovy
+++ src/tcgwars/logic/impl/gen8/SwordShieldPromos.groovy
@@ -553,59 +553,8 @@ public enum SwordShieldPromos implements LogicCardInfo {
           }
         }
       };
-      case GALARIAN_SIRFETCH_D_V_SWSH43:
-      return basic (this, hp:HP210, type:F, retreatCost:2) {
-        weakness P
-        bwAbility "Resolute Spear", {
-          text "Once during your turn, when this Pokémon moves from your Bench to the Active Spot, you may move any amount of [F] Energy from your other Pokémon to it."
-          delayedA {
-            after SWITCH, {
-              if (keyStore("$thisAbility", self, null) != bg.turnCount && self.active && bg.currentTurn == self.owner && ef.switchedOut==self && confirm("Use $thisAbility?")) {
-                keyStore("$thisAbility", self, bg.turnCount)
-                powerUsed()
-              }
-            }
-          }
-        }
-        move "Meteor Smash", {
-          text "200 damage. During your next turn, this Pokémon can't attack."
-          energyCost F, F, C
-          onAttack {
-            damage 200
-            cantAttackNextTurn self
-          }
-        }
-      };
-      case ETERNATUS_V_SWSH44:
-      return copy(DarknessAblaze.ETERNATUS_V_116, this);
-      case ETERNATUS_VMAX_SWSH45:
-      return copy(DarknessAblaze.ETERNATUS_VMAX_117, this);
-      case ELDEGOSS_SWSH46:
-      return copy(SwordShield.ELDEGOSS_21, this);
-      case DREDNAW_SWSH47:
-      return copy(SwordShield.DREDNAW_61, this);
-      case CENTISKORCH_SWSH48:
-      return copy(SwordShield.CENTISKORCH_39, this);
-      case DUBWOOL_V_SWSH49:
-      return copy(RebelClash.DUBWOOL_V_153, this);
       case CHARIZARD_V_SWSH50:
       return copy(DarknessAblaze.CHARIZARD_V_19, this);
-      case LAPRAS_SWSH51:
-      return copy(SwordShield.LAPRAS_48, this);
-      case GENGAR_SWSH52:
-      return copy(SwordShield.GENGAR_85, this);
-      case MACHAMP_SWSH53:
-      return copy(ChampionsPath.MACHAMP_26, this);
-      case COALOSSAL_SWSH54:
-      return copy (RebelClash.COALOSSAL_107, this);
-      case HATTERENE_V_SWSH55:
-      case MORPEKO_SWSH56:
-      case GRIMMSNARL_V_SWSH57:
-      case _SWSH58:
-      case _SWSH59:
-      case _SWSH60:
-      case _SWSH61:
-      case _SWSH62:
       case PIKACHU_V_SWSH63:
       return basic(this, hp:HP190, type:L, retreatCost:1) {
         weakness F
