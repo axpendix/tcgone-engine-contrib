62b4d14b48d4c99a643799f89b709ae4b8d91f85
Vincent Cunningham <v.cunningham@flagrama.com>
2020-11-09 20:06:09 -0500

    Make Shiftry whirlwind as a choice

--- src/tcgwars/logic/impl/gen8/AmazingVoltTackle.groovy
+++ src/tcgwars/logic/impl/gen8/AmazingVoltTackle.groovy
@@ -277,6 +277,7 @@ public enum AmazingVoltTackle implements LogicCardInfo {
           energyCost G, C
           onAttack {
             damage 110
+            if (!confirm("Have your opponent switch their Active Pokémon?"))
             whirlwind()
           }
         }
