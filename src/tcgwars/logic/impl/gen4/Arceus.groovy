095a52f45894015dc7f1ab8feb71ed45d2e77fc2
ufodynasty <39018953+ufodynasty@users.noreply.github.com>
2020-12-20 02:13:25 -0600

    final arceus lvx fix (#1040)

--- src/tcgwars/logic/impl/gen4/Arceus.groovy
+++ src/tcgwars/logic/impl/gen4/Arceus.groovy
@@ -2593,6 +2593,7 @@ public enum Arceus implements LogicCardInfo {
             text "Arceus can use the attacks of all Arceus you have in play as its own."
             getterA (GET_MOVE_LIST, self) {holder->
               def omniscientMoves = []
+              def cardList = []
               self.owner.pbg.bench.findAll {it.name == "Arceus"}.each {
                 if(self.active && !cardList.contains("${it.topPokemonCard}")){
                   cardList.add("${it.topPokemonCard}")
