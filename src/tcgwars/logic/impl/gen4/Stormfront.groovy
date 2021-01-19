8e3ab3fa6a9743f74e65a7d295f179695d7e7d30
unknown <ufodynasty12@gmail.com>
2021-01-19 14:14:17 -0600

    heatran and raichu debugging

--- src/tcgwars/logic/impl/gen4/Stormfront.groovy
+++ src/tcgwars/logic/impl/gen4/Stormfront.groovy
@@ -2981,41 +2981,35 @@ public enum Stormfront implements LogicCardInfo {
             text "Your opponent can’t remove the Special Condition Burned by evolving or devolving his or her Burned Pokémon. (This also includes putting a Pokémon Level-Up card onto the Burned Pokémon.) Whenever your opponent flips a coin for the Special Condition Burned between turns, treat it as tails."
             delayedA {
               before BURNED_SPC, null, null, EVOLVE, {
-                bc "Full Flame prevents removing the Special Condition Burned by evolving or devolving"
+                bc "Heat Metal prevents removing the Special Condition Burned by evolving or devolving"
                 prevent()
               }
               before BURNED_SPC, null, null, DEVOLVE, {
-                bc "Full Flame prevents removing the Special Condition Burned by evolving or devolving"
+                bc "Heat Metal prevents removing the Special Condition Burned by evolving or devolving"
                 prevent()
               }
-              before ASLEEP_SPC, null, null, BEGIN_TURN, {
-                delayed(inline: true){
-                  def doit = {
-                    if (bg.currentThreadPlayerType != self.owner) {
-                      bc "Heat Metal forced the coinflip to be TAILS."
-                      bg.deterministicCoinFlipQueue.offer(false)
-                    }
-                  }
-                  before COIN_FLIP, {doit()}
-                  before COIN_FLIP_GETTER, {doit()}
+              def flag
+              before BURNED_SPC, null, null, BEGIN_TURN, {
+                flag = true
+                bc "flag : $flag"
+              }
+              after BURNED_SPC, null, null, BEGIN_TURN, {
+                flag = false
+                bc "flag : $flag"
+              }
+              def doit = {
+                if (bg.currentThreadPlayerType != self.owner && flag) {
+                  bc "Heat Metal forced the coin flip to be TAILS."
+                  bg.deterministicCoinFlipQueue.offer(false)
                 }
               }
-            }
-          }
-          customAbility {
-            delayed {
-              after DISCARD_ENERGY {
-                bc "ef.card + $ef.card"
-                bc "ef.resolvedTarget + $ef.resolvedTarget"
-              }
+              before COIN_FLIP, {doit()}
+              before COIN_FLIP_GETTER, {doit()}
             }
           }
           pokePower "Heat Wave", {
             text "Once at the end of your turn, if Heatran is on your Bench, you may use this power. If you discarded basic Energy cards attached to your [R] or [M] Active Pokémon by that Pokémon’s attack this turn, attach up to 2 of those Energy cards to that Pokémon."
             delayedA {// TODO
-              after ATTACK_MAIN {
-                bc "After ATTACK_MAIN"
-              }
             }
           }
         };
@@ -3072,23 +3066,21 @@ public enum Stormfront implements LogicCardInfo {
         return levelUp (this, from:"Raichu", hp:HP110, type:LIGHTNING, retreatCost:0) {
           weakness F
           resistance M, MINUS20
-          def flag = false
-          customAbility {// If Link Lightning Knocks out a Pokémon that blocks abilities, Raichu can then attack again
-            onActivate {r ->
-              if(r == PLAY_FROM_HAND) {
-                flag = true
-              }
-            }
-          }
           pokeBody "Link Lightning", {
             text "Once during your turn, when you put Raichu LV.X onto Raichu and use Voltage Shoot, you may use another attack of Raichu afterward. This power can’t be used if Raichu is affected by a Special Condition."
             delayedA {
               after ATTACK_MAIN, {
-                if (ef.move.name == "Voltage Shoot" && !self.specialConditions && flag) {
+                bc "Here 1"
+                if (ef.move.name == "Voltage Shoot" && !self.specialConditions) {
+                  bc "Here 2"
+                  if(self.lastEvolved == bg.turnCount) {
+                    bc "Lvl up works with self.lastEvolved"
+                  }
                   powerUsed()
                   def moveList = []
                   moveList.addAll(self.topPokemonCard.moves)
                   moveList.addAll(self.topNonLevelUpPokemonCard.moves)//TODO: This breaks with Technical Machines. I wonder if the testers will notice?
+                  bc "Here 3"
                   def bef=blockingEffect(BETWEEN_TURNS)
                   attack (move as Move)
                   bef.unregisterItself(bg().em())
