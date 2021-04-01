7013de4e4b2b1c03070dc71538d9721d622e3690
Vincent Cunningham <v.cunningham@flagrama.com>
2021-04-01 17:38:20 -0400

    BST131: Fix attackRequirement check

--- src/tcgwars/logic/impl/gen8/BattleStyles.groovy
+++ src/tcgwars/logic/impl/gen8/BattleStyles.groovy
@@ -3007,7 +3007,7 @@ public enum BattleStyles implements LogicCardInfo {
             text "This attack does 30 damage to each of your opponent's Pokémon. (Don't apply Weakness and Resistance for Benched Pokémon.)"
             attackRequirement {
               // self is not set properly creating a move like this, use bg.ownActive() instead
-              assert bg.ownActive().singleStrike : "${bg.ownActive()} is not a $RAPID_STRIKE Pokémon"
+              assert bg.ownActive().rapidStrike : "${bg.ownActive()} is not a $RAPID_STRIKE Pokémon"
             }
             energyCost C, C
             onAttack {
