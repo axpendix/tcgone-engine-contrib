eaf0f20cf60a3a8d4692a34c66cf9eebdc43d4f7
Thomas Luong <luongthomasdev@gmail.com>
2021-04-07 12:18:29 -0700

    Update KINGDRA_33 ability to only activate if ko'd pokemon is active

--- src/tcgwars/logic/impl/gen8/BattleStyles.groovy
+++ src/tcgwars/logic/impl/gen8/BattleStyles.groovy
@@ -937,9 +937,9 @@ public enum BattleStyles implements LogicCardInfo {
           delayedA {
             before KNOCKOUT, {
               def energies = self.owner.pbg.active.cards.filterByEnergyType(W)
-              if ((ef as Knockout).byDamageFromAttack && bg.currentTurn == self.owner.opposite && !self.active && ef.pokemonToBeKnockedOut.owner == self.owner && ef.pokemonToBeKnockedOut.active && energies && oppConfirm("Use $thisAbility?")) {
+              if ((ef as Knockout).byDamageFromAttack && bg.currentTurn == self.owner.opposite && !self.active && ef.pokemonToBeKnockedOut.owner == self.owner && ef.pokemonToBeKnockedOut.active && energies && confirm("Use $thisAbility?", self.owner)) {
                 powerUsed()
-                def energiesToMove = energies.oppSelect(max: energies.size(), "Which Energies to move to Kingdra?")
+                def energiesToMove = energies.select(max: energies.size(), playerType: self.owner, "Which Energies to move to Kingdra?")
 
                 def pcsMap = [:]
 
