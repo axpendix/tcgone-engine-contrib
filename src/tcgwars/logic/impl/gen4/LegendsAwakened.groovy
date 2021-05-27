19d395ef6985e51d7a3c79191c705af72a001549
Guillermo Bianchi <ultraguille2006@gmail.com>
2021-05-27 12:58:51 -0300

    Fix Luxray (LA)'s Rivalry
    Should increase by 50, not 30

--- src/tcgwars/logic/impl/gen4/LegendsAwakened.groovy
+++ src/tcgwars/logic/impl/gen4/LegendsAwakened.groovy
@@ -465,11 +465,11 @@ public enum LegendsAwakened implements LogicCardInfo {
             text "If your opponent has any Pokémon LV.X in play, each of Luxray's attacks does 50 more damage to the Active Pokémon (before applying Weakness and Resistance)."
             delayedA {
               after PROCESS_ATTACK_EFFECTS, {
-                if (opp.all.any { it.isPokemonLevelUp() }) {
+                if (ef.attacker == self && opp.all.any { it.isPokemonLevelUp() }) {
                   bg.dm().each {
-                    if (it.from == self && it.to.active && it.to.owner != self.owner && it.dmg.value && it.notNoEffect) {
-                      bc "Rivalry +30"
-                      it.dmg += hp(30)
+                    if (it.to.active && it.notZero) {
+                      bc "Rivalry +50"
+                      it.dmg += hp(50)
                     }
                   }
                 }
