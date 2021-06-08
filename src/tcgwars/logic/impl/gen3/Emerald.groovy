0f1195f96026374e4774d6331517a3eea8ab3d1a
Guillermo Bianchi <ultraguille2006@gmail.com>
2021-06-08 17:13:21 -0300

    Actually fix Cacturne-ex (EM), alongside Dusclops (CG)
    Should now properly block special energies. Adapted based on Stormfront's shiny Duskull's attack (which seems to block properly, save for not ignoring Dark/Metal special energy)

--- src/tcgwars/logic/impl/gen3/Emerald.groovy
+++ src/tcgwars/logic/impl/gen3/Emerald.groovy
@@ -1930,8 +1930,8 @@ public enum Emerald implements LogicCardInfo {
             text "As long as Cacturne ex is your Active Pokémon, your opponent can’t attach any Special Energy cards (except for [D] and [M] Energy cards) from his or her hand to his or her Active Pokémon."
             delayedA {
               before ATTACH_ENERGY, self.owner.opposite.pbg.active, {
-                if(ef.reason == PLAY_FROM_HAND && ef.resolvedTarget.owner == self.owner.opposite && ef.resolvedTarget.active && self.active && (ef.card.cardTypes.is(SPECIAL_ENERGY) && ef.card.name != "Darkness Energy" && ef.card.name != "Metal Energy")) {
-                  wcu "Cursed Glare: Can't attach energy"
+                if(ef.reason == PLAY_FROM_HAND && self.active && (ef.card.cardTypes.is(SPECIAL_ENERGY) && ef.card.name != "Darkness Energy" && ef.card.name != "Metal Energy")) {
+                  wcu "$thisAbility: Can't attach special energies to your Active Pokémon"
                   prevent()
                 }
               }
