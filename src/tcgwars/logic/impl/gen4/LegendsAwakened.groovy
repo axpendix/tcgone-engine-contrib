7191dfebd142156ab0f3b98e2972363a3520a07e
Thomas Luong <luongthomasdev@gmail.com>
2021-06-08 11:52:04 -0700

    Fix HOUNDOOM_57 Revenge Fang Pokebody to not crash when attacking

--- src/tcgwars/logic/impl/gen4/LegendsAwakened.groovy
+++ src/tcgwars/logic/impl/gen4/LegendsAwakened.groovy
@@ -2046,9 +2046,11 @@ public enum LegendsAwakened implements LogicCardInfo {
             text "If you have less Benched Pokémon than your opponent, each of Houndoom's attacks does 40 more damage to the Active Pokémon (before applying Weakness and Resistance)."
             delayedA {
               after PROCESS_ATTACK_EFFECTS, {
-                if (ef.attacker == self && my.bench.size() < opp.bench.size()) {
-                  bc "Revenge Fang adds 40 damage"
-                  it.dmg += hp(40)
+                bg.dm().each {
+                  if (it.from == self && it.to.active && it.to.owner != self.owner && it.dmg.value && my.bench.size() < opp.bench.size()) {
+                    bc "Revenge Fang +40"
+                    it.dmg += hp(40)
+                  }
                 }
               }
             }
