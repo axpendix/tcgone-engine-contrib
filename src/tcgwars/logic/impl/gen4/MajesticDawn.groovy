2197136e45e696366027e56b7a6f3a3b36d9f438
Thomas Luong <luongthomasdev@gmail.com>
2021-06-09 15:58:45 -0700

    Arceus Set Bugfixes (#1267)
    * Update SHINX_SH12 weakness to plus10
    
    * Update BAGON_SH10 weakness to plus10
    
    * DEPARTMENT_STORE_GIRL_85 add deck shuffling after effect
    
    * Update PIKACHU_71 Mega Shot attack to discard energies after damage calculations
    
    * Update ELECTRIKE_63 Weakness to be plus10
    
    * Update GASTLY_64 Weakness to be plus10
    
    * Update GASTLY_36 Weakness to be plus10
    
    * Update ELECTRIKE_62 Weakness to be plus10
    
    * Update FROSLASS_2 Snow Gift pokepower to shuffle deck after search
    
    * Update AERODACTYL_13 Unearth pokepower to shuffle deck after search
    
    * Update PONYTA_46 weakness to plus10
    
    * Update CHARIZARD_1 Burning Tail attack cost to RRC
    
    * Update PICHU_25 Baby Steps attack to not reveal card that was placed into hand
    
    * Update HELIX_FOSSIL_91 and HELIX_FOSSIL_93 Aqua Reaction Pokebody to not be optional
    
    * Update DOME_FOSSIL_89 and DOME_FOSSIL_92 Rock Reaction Pokebody to not be optional
    
    * Fix ARCEUS_AR4 Fastwave attack to be affected by weakness
    
    * Update BEGINNING_DOOR_82 to allow searching of Arceus Lv.X cards

--- src/tcgwars/logic/impl/gen4/MajesticDawn.groovy
+++ src/tcgwars/logic/impl/gen4/MajesticDawn.groovy
@@ -2429,7 +2429,7 @@ public enum MajesticDawn implements LogicCardInfo {
               pokeBody "Rock Reaction", {
                 delayedA{
                   after ATTACH_ENERGY, self, {
-                    if(ef.reason==PLAY_FROM_HAND && ef.card.asEnergyCard().containsType(F) && self.owner.pbg.deck && confirm("Use Rock Reaction?")){
+                    if (ef.reason==PLAY_FROM_HAND && ef.card.asEnergyCard().containsType(F) && self.owner.pbg.deck) {
                       def sel=my.deck.search("Search your deck for a card that evolved from $self",{it.cardTypes.is(EVOLUTION) && it.predecessor==self.name})
                       if(sel){
                         evolve(self, sel.first(), OTHER)
@@ -2500,7 +2500,7 @@ public enum MajesticDawn implements LogicCardInfo {
               pokeBody "Aqua Reaction", {
                 delayedA{
                   after ATTACH_ENERGY, self, {
-                    if(ef.reason==PLAY_FROM_HAND && ef.card.asEnergyCard().containsType(W) && self.owner.pbg.deck && confirm("Use Rock Reaction?")){
+                    if (ef.reason==PLAY_FROM_HAND && ef.card.asEnergyCard().containsType(W) && self.owner.pbg.deck) {
                       def sel=my.deck.search("Search your deck for a card that evolved from $self",{it.cardTypes.is(EVOLUTION) && it.predecessor==self.name})
                       if(sel){
                         evolve(self, sel.first(), OTHER)
