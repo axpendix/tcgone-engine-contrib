51b23b44f2a5cb9a0afb910ee6d30dee4d4f5b12
unknown <ufodynasty12@gmail.com>
2022-04-14 13:47:56 -0500

    Revert "Fix Gathering of Cats interaction with move duplicators"
    This reverts commit 740ce74990a4119ed7956e47f6dad671de8572c4.

--- src/tcgwars/logic/impl/gen7/UnbrokenBonds.groovy
+++ src/tcgwars/logic/impl/gen7/UnbrokenBonds.groovy
@@ -308,7 +308,7 @@ public enum UnbrokenBonds implements LogicCardInfo {
 
   @Override
   public String getEnumName() {
-    return name();
+    return this.name();
   }
 
   @Override
@@ -1322,7 +1322,7 @@ public enum UnbrokenBonds implements LogicCardInfo {
             text "100x damage. Flip 3 coins. This attack does 100 damage for each heads. If all of them are tails, you lose this game."
             energyCost W, C, C
             onAttack {
-              flip 3,{},{},[0:{bg.getGame().endGame(opp.active.owner, WinCondition.OTHER)},1:{damage 100},2:{damage 200},3:{damage 300}]
+              flip 3,{},{},[0:{bg.getGameManager().endGame(opp.active.owner, WinCondition.OTHER)},1:{damage 100},2:{damage 200},3:{damage 300}]
             }
           }
 
@@ -1635,7 +1635,7 @@ public enum UnbrokenBonds implements LogicCardInfo {
                   bg.em().run(new ChangeImplementation(pkmnCard, energyCard))
                 }
               }
-              energyCard.player = thisCard.player
+              energyCard.initializeFrom thisCard
               bg.em().run(new ChangeImplementation(energyCard, pkmnCard))
               attachEnergy(pcs, energyCard)
               bc "$energyCard is now a Special Energy Card that provides 2 [L] energy attached to $pcs"
@@ -3636,7 +3636,7 @@ public enum UnbrokenBonds implements LogicCardInfo {
           weakness F
           bwAbility "Gathering of Cats", {
             text "Ignore all Energy in the attack costs of each of your Pokémon in play that has the Caturday attack."
-            getterA (GET_MOVE_LIST, BEFORE_LAST) {h->
+            getterA GET_MOVE_LIST, {h->
               PokemonCardSet pcs = h.effect.target
               if(pcs.owner==self.owner && h.object.find{it.name=='Caturday'}){
                 def list=[]
