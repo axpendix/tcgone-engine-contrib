56ed0ac9766d2a123a4dcb664b0199c63172a3b6
axpendix <axpendix@hotmail.com>
2023-03-12 16:18:33 +0100

    feat: shift Rocket's Sneak Attack (Team Rocket) to groovy

--- src/tcgwars/logic/impl/gen1/TeamRocketNG.groovy
+++ src/tcgwars/logic/impl/gen1/TeamRocketNG.groovy
@@ -619,9 +619,10 @@ public enum TeamRocketNG implements LogicCardInfo {
         return basicTrainer (this) {
           text "Look at your opponent’s hand. If he or she has any Trainer cards, choose 1 of them. Your opponent shuffles that card into his or her deck."
           onPlay {
-            def list = opp.hand.shuffledCopy().showToMe("Opponent's hand").filterByType(TRAINER)
-            if(list){
-              list.select(count: 1, "Discard").moveTo(opp.deck)
+            def list = opp.hand.shuffledCopy().showToMe("Opponent's hand")
+            list = list.filterByType(TRAINER)
+            if(list) {
+              list.select(count: 1, "Shuffle to deck?").showToOpponent("Opponent played Rocket's Sneak Attack and this card from your hand will be shuffled into your deck").moveTo(opp.deck)
               shuffleDeck(null, TargetPlayer.OPPONENT)
             }
           }
