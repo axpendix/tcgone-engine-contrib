09ecf854c2c0a889dde49f14dfd39a956245ff5a
dxdydzd <160867249+dxdydzd@users.noreply.github.com>
2025-09-11 23:15:14 +0800

    Haunter: Time Spiral will check all Pokémon, not just Benched

--- src/tcgwars/logic/impl/gen3/LegendMaker.groovy
+++ src/tcgwars/logic/impl/gen3/LegendMaker.groovy
@@ -1323,7 +1323,7 @@ public enum LegendMaker implements LogicCardInfo {
           text "If your opponent has any Evolved Pokémon in play, choose 1 of them and flip a coin. If heads, remove the highest Stage Evolution card on that Pokémon and have your opponent shuffled it into his or her deck."
           energyCost C, C
           attackRequirement {
-            assert opp.bench.any{ it.evolution } : "Opponent has no evolved Pokémon"
+            assert opp.all.any{ it.evolution } : "Opponent has no evolved Pokémon"
           }
           onAttack {
             def list = opp.all.findAll { it.evolution }
